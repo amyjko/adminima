9f5670230097ad5be8b9e6e75e67964d868c535b
Amy J. Ko <ajko@uw.edu>
2023-12-01 20:51:33 -0800

    Extracted RoleView.

---
+++ src/lib/RoleView.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type Role from '../types/Role';
+	import Header from '$lib/Header.svelte';
+	import Paragraph from '$lib/Paragraph.svelte';
+	import Time from '$lib/ActivitiesView.svelte';
+	import type Activity from '../types/Activity';
+
+	export let role: Role;
+	export let activities: Activity[];
+</script>
+
+<div class="scope">
+	<div class="meta">
+		<Header>{role.title}</Header>
+		<Paragraph>{role.what}</Paragraph>
+	</div>
+	<Time {activities} />
+</div>
+
+<style>
+	.scope {
+		display: flex;
+		flex-direction: column;
+		width: 100vw;
+		height: 100vh;
+	}
+
+	.meta {
+		padding: var(--spacing);
+	}
+</style>
--- src/routes/role/+page.svelte
+++ src/routes/role/+page.svelte
@@ -1,34 +1,12 @@
 <script lang="ts">
-	import Header from '$lib/Header.svelte';
-	import Paragraph from '$lib/Paragraph.svelte';
-	import Time from '$lib/Time.svelte';
-
 	import MockActivities from '$lib/mock/activities.json?raw';
 	import MockRole from '$lib/mock/role.json?raw';
 	import type Role from '../../types/Role';
 	import type Activity from '../../types/Activity';
+	import RoleView from '$lib/RoleView.svelte';
 
 	const role = JSON.parse(MockRole) as Role;
 	const activities = JSON.parse(MockActivities) as Activity[];
 </script>
 
-<div class="scope">
-	<div class="meta">
-		<Header>{role.title}</Header>
-		<Paragraph>{role.what}</Paragraph>
-	</div>
-	<Time {activities} />
-</div>
-
-<style>
-	.scope {
-		display: flex;
-		flex-direction: column;
-		width: 100vw;
-		height: 100vh;
-	}
-
-	.meta {
-		padding: var(--spacing);
-	}
-</style>
+<RoleView {role} {activities} />
