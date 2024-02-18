a8c47c967cb4006359703504e90383b2dffce3ca
Amy J. Ko <ajko@uw.edu>
2024-02-18 14:52:28 -0800

    Simpler landing page for organization.

--- src/lib/OrganizationView.svelte
+++ src/lib/OrganizationView.svelte
@@ -1,29 +1,32 @@
 <script lang="ts">
 	import type Organization from '../types/Organization';
-	import PersonLink from './PersonLink.svelte';
-	import Header from '$lib/Header.svelte';
-	import database from '../database/Database';
-	import Button from './Button.svelte';
-	import { withoutAdmin } from '../types/Organization';
 	import { user } from '../database/Auth';
 	import MarkupView from './MarkupView.svelte';
 	import Link from './Link.svelte';
 	import Title from './Title.svelte';
 	import { locale } from '$types/Locales';
-	import Flow from './Flow.svelte';
+	import Paragraph from './Paragraph.svelte';
 
 	export let organization: Organization;
-
-	$: isAdmin = organization.admins.includes($user.id);
 </script>
 
 <Title title={organization.name} kind={$locale?.term.organization} />
 
 <MarkupView markup={organization.description} />
 
-<Flow>
-	<Link to="{organization.id}/roles">Roles</Link>
-	<Link to="{organization.id}/people">People</Link>
-	<Link to="{organization.id}/processes">Processes</Link>
-	<Link to="{organization.id}/changes">Changes</Link>
-</Flow>
+<Paragraph>
+	Learn what <Link to="{organization.id}/roles">Roles</Link> people have here.</Paragraph
+>
+
+<Paragraph
+	>Find all of the <Link to="{organization.id}/people">People</Link> in this organization.</Paragraph
+>
+
+<Paragraph
+	>See all of the <Link to="{organization.id}/processes">Processes</Link> that make this organization
+	work.</Paragraph
+>
+
+<Paragraph
+	>Suggest a <Link to="{organization.id}/changes">Change</Link> to make this organization work better.</Paragraph
+>
