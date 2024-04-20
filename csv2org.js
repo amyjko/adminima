562044b9c2fb228cdcc4109b588a1a8f8dd1cd24
Amy J. Ko <ajko@uw.edu>
2024-04-20 14:18:59 -0700

    More elaborate mock data.

---
+++ csv2org.js
@@ -0,0 +1,149 @@
+import fs from 'fs';
+import fastcsv from 'fast-csv';
+
+if (process.argv.length < 2) {
+	console.error('Usage: node csv2org.js <filename>');
+	process.exit(1);
+}
+
+const filename = process.argv[2];
+const orgName = filename.split('-').at(-1).trim().split('.')[0];
+
+const stream = fs.createReadStream(process.argv[2]);
+
+const rows = [];
+let headers = null;
+const csv = fastcsv
+	.parse({ headers: true })
+	.on('data', function (row) {
+		rows.push(row);
+	})
+
+	.on(
+		'end',
+
+		function () {
+			convert(rows);
+		}
+	)
+	.on('headers', (heads) => {
+		headers = heads;
+	})
+	.on('error', function (error) {
+		console.error(error);
+	});
+
+function roleNameToID(name) {
+	return name.replaceAll(' ', '').toLowerCase();
+}
+
+stream.pipe(csv);
+
+function convert(rows) {
+	const withoutTallies = rows.slice(4);
+
+	const processes = [];
+	const concerns = [];
+	for (const row of withoutTallies) {
+		if (/Proposed|Discussed|Pending|Final/.test(row['Status'])) {
+			processes.push({
+				id: '' + processes.length,
+				icon: '',
+				organization: 'ischool',
+				concern: concerns.at(-1).id,
+				start: null,
+				repeat: null,
+				title: row['Task'],
+				status: row['Status'],
+				accountable: roleNameToID(Object.keys(row).find((key) => row[key] === 'A') ?? ''),
+				responsible: Object.keys(row)
+					.filter((key) => row[key] === 'R')
+					.map((name) => roleNameToID(name)),
+				consulted: Object.keys(row)
+					.filter((key) => row[key] === 'C')
+					.map((name) => roleNameToID(name)),
+				informed: Object.keys(row)
+					.filter((key) => row[key] === 'I')
+					.map((name) => roleNameToID(name)),
+				what: row['Notes'],
+				how: [],
+				visibility: 'public',
+				modifications: []
+			});
+		} else {
+			const [name, description] = row['Status'].split('\n');
+			concerns.push({ id: name.toLowerCase(), name, description });
+		}
+	}
+
+	const roles = [];
+	// Skip the first three columns
+	for (const header of headers.slice(3)) {
+		roles.push({
+			id: roleNameToID(header),
+			organization: 'ischool',
+			title: header,
+			what: '',
+			people: [],
+			status: 'Proposed',
+			visibility: 'public',
+			modifications: []
+		});
+	}
+
+	const data = {
+		organizations: [
+			{
+				id: 'ischool',
+				name: orgName,
+				description: '',
+				admins: [],
+				staff: [],
+				teams: [],
+				concerns: concerns,
+				statuses: [
+					{
+						id: '1',
+						name: 'Drafted',
+						description: 'Still working on it'
+					},
+					{
+						id: '2',
+						name: 'Discussed',
+						description: 'Discussed with stakeholders'
+					},
+					{
+						id: '3',
+						name: 'Proposed',
+						description: 'Ready for feedback'
+					},
+					{
+						id: '4',
+						name: 'Approved',
+						description: 'Approved and ready to implement'
+					},
+					{
+						id: '4',
+						name: 'Implemented',
+						description: 'Implemented'
+					}
+				],
+				visibility: 'public',
+				modifications: []
+			}
+		],
+		processes,
+		roles,
+		changes: [],
+		people: []
+	};
+
+	const jsonData = JSON.stringify(data);
+	fs.writeFile('src/database/mock.json', jsonData, (err) => {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log('Data written to file successfully.');
+		}
+	});
+}
--- src/routes/organization/[orgid]/+layout.server.ts
+++ src/routes/organization/[orgid]/+layout.server.ts
@@ -1,20 +1,35 @@
-import data from '$database/Mock.js';
+import data from '$database/mock.json';
+import type Change from '$types/Change';
+import type Organization from '$types/Organization';
+import type Person from '$types/Person';
+import type Process from '$types/Process';
+import type Role from '$types/Role';
+
+type MockData = {
+	roles: Role[];
+	organizations: Organization[];
+	people: Person[];
+	changes: Change[];
+	processes: Process[];
+};
+
+const mock = data as unknown as MockData;
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ params }) {
 	const orgid = params.orgid;
 
-	const org = data.organizations.find((org) => org.id === orgid);
+	const org = mock.organizations.find((org) => org.id === orgid);
 
 	if (org) {
 		return {
 			payload: org
 				? {
 						organization: org,
-						roles: data.roles.filter((role) => role.organization === orgid),
-						people: data.people.filter((person) => org.staff.includes(person.id)),
-						processes: data.processes.filter((task) => task.organization === orgid),
-						changes: data.changes.filter((change) => change.organization === orgid)
+						roles: mock.roles.filter((role) => role.organization === orgid),
+						people: mock.people.filter((person) => org.staff.includes(person.id)),
+						processes: mock.processes.filter((task) => task.organization === orgid),
+						changes: mock.changes.filter((change) => change.organization === orgid)
 				  }
 				: null
 		};
