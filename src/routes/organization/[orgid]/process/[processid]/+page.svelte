4e4ef61ed12d1bf0b94cfae62ce61c4b1ed443bc
Amy J. Ko <ajko@uw.edu>
2024-06-02 11:03:32 -0700

    Removed redundant process view.

--- src/routes/organization/[orgid]/process/[processid]/+page.svelte
+++ src/routes/organization/[orgid]/process/[processid]/+page.svelte
@@ -1,16 +1,112 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import MarkupView from '$lib/MarkupView.svelte';
+	import Header from '$lib/Header.svelte';
+	import TaskView from '$lib/TaskView.svelte';
+	import Organizations, { type ProcessRow } from '$database/Organizations';
 	import Oops from '$lib/Oops.svelte';
-	import ProcessView from '$lib/ProcessView.svelte';
+	import ChangeForm from '$lib/ChangeForm.svelte';
+	import Button from '$lib/Button.svelte';
+	import Paragraph from '$lib/Paragraph.svelte';
+	import { goto } from '$app/navigation';
+	import Admin from '$lib/Admin.svelte';
+	import Title from '$lib/Title.svelte';
+	import { locale } from '$types/Locales';
+	import RoleContribution from '$lib/RoleContribution.svelte';
+	import Level from '$lib/Level.svelte';
 	import { getOrg } from '$lib/contexts';
+	import Status from '$lib/Status.svelte';
+	import CommentsView from '$lib/CommentsView.svelte';
+	import { page } from '$app/stores';
+
+	let deleteError: string | undefined = undefined;
 
 	const org = getOrg();
-
 	$: process = $org.getProcess($page.params.processid);
+	$: how = process ? $org.getHow(process.id) : undefined;
 </script>
 
-{#if process}
-	<ProcessView {process} />
-{:else}
+{#if process === null}
 	<Oops text={(locale) => locale.error.noProcess} />
+{:else}
+	<Title title={process.what} kind={$locale?.term.process ?? ''}>
+		<Status status={process.status} />
+	</Title>
+
+	<MarkupView markup={process.what} unset="No description yet." />
+
+	<Paragraph><em>Area of concern</em>: <strong>{process.concern}</strong></Paragraph>
+
+	<Header>Who</Header>
+
+	{#if how}
+		<Paragraph>
+			<Level level="accountable" /><strong>ccountable</strong>{#if how?.responsible.length === 0}and <strong
+					>responsible</strong
+				>{/if} for this task's outcomes:
+			<RoleContribution roles={how.accountable ? [how.accountable] : []} />
+		</Paragraph>
+		{#if how.responsible.length > 0}
+			<Paragraph>
+				<Level level="responsible" /><strong>esponsible</strong> for completing this process: <RoleContribution
+					roles={how.responsible}
+				/>
+			</Paragraph>
+		{/if}
+		{#if how.consulted.length > 0}
+			<Paragraph>
+				<Level level="consulted" /><strong>onsulted</strong> in this process: <RoleContribution
+					roles={how.consulted}
+				/>
+			</Paragraph>
+		{/if}
+		{#if how.informed.length > 0}
+			<Paragraph>
+				<Level level="informed" /><strong>nformed</strong> that this process is happening and of its
+				outcome: <RoleContribution roles={how.informed} />
+			</Paragraph>
+		{/if}
+	{/if}
+
+	<Header>When</Header>
+
+	<!-- {#if process.repeat}
+	{#if process.repeat.type === 'monthly'}
+		This happens on the {process.repeat.date}st day of each month.
+	{:else if process.repeat.type === 'weekly'}
+		This happens every {process.repeat.weekday} of each week.
+	{:else if process.repeat.type === 'annually'}
+		This happens on day {process.repeat.day} of month {process.repeat.month} each year.
+	{/if}
+{:else}
+	This process doesn't happen at a particular time.
+{/if} -->
+
+	<Header>How</Header>
+
+	{#if how === undefined}
+		<p>This process doesn't have any tasks listed yet.</p>
+	{:else}
+		<TaskView {how} />
+	{/if}
+
+	<ChangeForm process={process.id} />
+
+	<Admin>
+		<Paragraph>Is this process obsolete? You can permanently delete it.</Paragraph>
+		<Button
+			action={async () => {
+				try {
+					const org = process.orgid;
+					await Organizations.deleteProcess(process.id);
+					goto(`/organization/${org}/processes`);
+				} catch (_) {
+					deleteError = "We couldn't delete this";
+				}
+			}}
+			warning>Delete this process</Button
+		>
+		{#if deleteError}<Oops text={deleteError} />{/if}
+	</Admin>
+
+	<CommentsView comments={process.comments} />
 {/if}
