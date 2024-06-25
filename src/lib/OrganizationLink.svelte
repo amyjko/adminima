e55907cfc5b3faeab4beb5ee3c85daeecd1c04e0
Amy J. Ko <ajko@uw.edu>
2024-06-24 17:58:14 -0700

    Loading feedback on link.

--- src/lib/OrganizationLink.svelte
+++ src/lib/OrganizationLink.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import Link from './Link.svelte';
 	import { type OrganizationID } from '$types/Organization';
+	import { navigating } from '$app/stores';
+	import Loading from './Loading.svelte';
 
 	export let id: OrganizationID;
 	export let name: string;
 </script>
 
-<Link to="/org/{id}" kind="org">{name}</Link>
+{#if $navigating}
+	<Loading />
+{:else}
+	<Link to="/org/{id}" kind="org">{name}</Link>
+{/if}
