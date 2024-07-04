22421c33bea100d542e4b8e94ba98be9a0d45d38
Amy J. Ko <ajko@uw.edu>
2024-07-04 13:15:10 -0700

    Cleaned up landing page.

--- src/lib/OrganizationView.svelte
+++ src/lib/OrganizationView.svelte
@@ -32,65 +32,57 @@
 				)
 		: undefined}
 >
-	<Visibility
-		tip="Change the visibility of this organization"
-		level={organization.getVisibility()}
-		edit={$user && editable
-			? (vis) =>
-					vis === 'org' || vis === 'admin' || vis === 'public'
-						? queryOrError(
-								errors,
-								$db.updateOrgVisibility(organization, vis, $user.id),
-								"Couldn't update organization visibility."
-						  )
-						: undefined
-			: undefined}
-	/>
-	<Note inline
-		>{#if organization.getVisibility() === 'public'}Everyone on the internet can see this
-			organization's details.{:else if organization.getVisibility() === 'org'}Only members can see
-			this organization's detail.{:else if organization.getVisibility() === 'admin'}Only admins can
-			see this organization's details.{/if}</Note
-	>
-</Title>
-
-<Tip
-	>This is an organization's dashboard. Learn about the organization, or edit it if you have
-	permissions.</Tip
+	<div class="meta">
+		<div class="links">
+			<Visibility
+				tip="Change the visibility of this organization"
+				level={organization.getVisibility()}
+				edit={$user && editable
+					? (vis) =>
+							vis === 'org' || vis === 'admin' || vis === 'public'
+								? queryOrError(
+										errors,
+										$db.updateOrgVisibility(organization, vis, $user.id),
+										"Couldn't update organization visibility."
+								  )
+								: undefined
+					: undefined}
+			/>
+			<Note inline
+				>{#if organization.getVisibility() === 'public'}Everyone on the internet can see this
+					organization's details.{:else if organization.getVisibility() === 'org'}Only members can
+					see this organization's detail.{:else if organization.getVisibility() === 'admin'}Only
+					admins can see this organization's details.{/if}</Note
+			>
+		</div>
+		<div class="links">
+			<span class="link">
+				<strong>{organization.getRoles().length}</strong>
+				<Link kind="role" to="/org/{organization.getID()}/roles"
+					>Role{organization.getRoles().length === 1 ? '' : 's'}</Link
+				>
+			</span>
+			<span class="link">
+				<strong>{organization.getProfiles().length} </strong>
+				<Link kind="person" to="/org/{organization.getID()}/people"
+					>{organization.getProfiles().length !== 1 ? 'People' : 'Person'}</Link
+				>
+			</span>
+			<span class="link">
+				<strong>{organization.getProcesses().length}</strong>
+				<Link kind="process" to="/org/{organization.getID()}/processes"
+					>Process{organization.getProcesses().length !== 1 ? 'es' : ''}</Link
+				>
+			</span>
+			<span class="link">
+				<strong>{organization.getSuggestions().length}</strong>
+				<Link kind="suggestion" to="/org/{organization.getID()}/suggestions"
+					>Suggestion{organization.getSuggestions().length !== 1 ? 's' : ''}</Link
+				>
+			</span>
+		</div>
+	</div></Title
 >
-
-<Paragraph>
-	See the <strong>{organization.getRoles().length}</strong>
-	<Link kind="role" to="/org/{organization.getID()}/roles"
-		>Role{organization.getRoles().length === 1 ? '' : 's'}</Link
-	> and <strong>{organization.getTeams().length}</strong> team{organization.getTeams().length === 1
-		? ''
-		: 's'} in this organization.</Paragraph
->
-
-<Paragraph
-	>See the <strong>{organization.getProfiles().length} </strong>
-	<Link kind="person" to="/org/{organization.getID()}/people"
-		>{organization.getProfiles().length !== 1 ? 'People' : 'Person'}</Link
-	> in this organization.</Paragraph
->
-
-<Paragraph
-	>See the <strong>{organization.getProcesses().length}</strong>
-	<Link kind="process" to="/org/{organization.getID()}/processes"
-		>Process{organization.getProcesses().length !== 1 ? 'es' : ''}</Link
-	>
-	that make this organization work.</Paragraph
->
-
-<Paragraph
-	>See the <strong>{organization.getSuggestions().length}</strong>
-	<Link kind="suggestion" to="/org/{organization.getID()}/suggestions"
-		>Suggestion{organization.getSuggestions().length !== 1 ? 's' : ''}</Link
-	> proposed.</Paragraph
->
-
-<hr />
 
 <MarkupView
 	markup={organization.getDescription()}
@@ -104,3 +96,17 @@
 	comments={organization.getComments()}
 	remove={(comment) => $db.deleteComment(organization.getRow(), 'orgs', comment)}
 />
+
+<style>
+	.meta {
+		display: flex;
+		flex-direction: column;
+		gap: var(--padding);
+	}
+	.links {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: var(--spacing);
+	}
+</style>
