b8fe1fc310fa8856f4f353b64a04140f9d893cd0
Amy J. Ko <ajko@uw.edu>
2024-07-05 19:20:48 -0700

    Cleaned up suggestion form design.

--- src/lib/SuggestionForm.svelte
+++ src/lib/SuggestionForm.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import Form from './Form.svelte';
 	import Oops from './Oops.svelte';
 	import { addError, getDB, getErrors, getOrg, getUser, queryOrError } from './contexts';
 	import type { ProcessID, RoleID } from '$types/Organization';
 	import Field from './Field.svelte';
 	import Button from './Button.svelte';
-	import Select from './Select.svelte';
 	import Labeled from './Labeled.svelte';
 	import MarkupView from './MarkupView.svelte';
+	import Tip from './Tip.svelte';
+	import Select from './Select.svelte';
 
 	export let process: ProcessID | undefined = undefined;
 	export let role: RoleID | undefined = undefined;
+
+	const defaultPrompt = 'Describe the problem';
 
 	let newRequestTitle = '';
 	let newRequestProblem = '';
@@ -21,6 +23,8 @@
 	const user = getUser();
 	const db = getDB();
 	const errors = getErrors();
+
+	$: isAdmin = $user && $organization.hasAdminPerson($user.id);
 
 	async function createSuggestion() {
 		if ($user === null) return;
@@ -44,25 +48,16 @@
 	$: active = newRequestTitle.length > 0 && newRequestProblem.length > 0;
 </script>
 
-<Form action={createSuggestion} {active} inactiveMessage="Add a title and problem.">
-	<MarkupView
-		markup={$organization.getPrompt()}
-		placeholder="What is your suggestion?"
-		edit={$user
-			? (text) =>
-					queryOrError(
-						errors,
-						$db.updateOrgPrompt($organization, text, $user.id),
-						"Couldn't update prompt"
-					)
-			: undefined}
-	/>
+<div class="form">
 	<Field label="Title" bind:text={newRequestTitle} />
 	<Labeled
-		label="Describe the problem you're experiencing and any ideas you have for addressing it."
+		label={$organization.getPrompt().trim().length === 0
+			? defaultPrompt
+			: $organization.getPrompt()}
 	>
 		<MarkupView bind:markup={newRequestProblem} editing placeholder="Detail your suggestion" />
 	</Labeled>
+
 	<Labeled label="Affected Roles">
 		<Select
 			tip="Add a role that is affected by this suggestion."
@@ -89,9 +84,36 @@
 			change={(p) => (process = p)}
 		/>
 	</Labeled>
-	<Button tip="Submit the new suggestion." submit end {active} action={() => {}}>create</Button>
 
-	{#if newRequestError}
-		<Oops text={newRequestError} />
-	{/if}
-</Form>
+	<Button tip="Submit the new suggestion." end {active} action={createSuggestion}>create</Button>
+</div>
+
+{#if newRequestError}
+	<Oops text={newRequestError} />
+{/if}
+
+{#if isAdmin}
+	<hr />
+	<Tip admin>Want to customize the prompt above?</Tip>
+	<MarkupView
+		markup={$organization.getPrompt()}
+		placeholder={'No custom prompt set'}
+		edit={$user
+			? (text) =>
+					queryOrError(
+						errors,
+						$db.updateOrgPrompt($organization, text, $user.id),
+						"Couldn't update prompt"
+					)
+			: undefined}
+	/>
+{/if}
+
+<style>
+	.form {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing);
+	}
+</style>
