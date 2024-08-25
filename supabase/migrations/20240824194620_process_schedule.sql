02b4b6d066465ef4ddab7684f4756dfceae5be4a
Amy J. Ko <ajko@uw.edu>
2024-08-24 17:55:04 -0700

    Fixed error in migration.

--- supabase/migrations/20240824194620_process_schedule.sql
+++ supabase/migrations/20240824194620_process_schedule.sql
@@ -1,3 +1,3 @@
-ALTER TABLE processes ALTER COLUMN repeat TYPE JSONB USING COALESCE(repeat, '[]'::jsonb);
+ALTER TABLE processes ALTER COLUMN repeat TYPE JSONB USING to_jsonb(COALESCE(repeat, '[]'::text));
 ALTER TABLE processes ALTER COLUMN repeat SET DEFAULT '[]'::jsonb;
 ALTER TABLE processes ALTER COLUMN repeat SET NOT NULL;
