0a71c2d27d51d214881f51d794f3caffda8b9a08
Amy J. Ko <ajko@uw.edu>
2024-10-12 08:10:12 -0700

    Simplified load.

--- src/routes/person/[personid]/+page.ts
+++ src/routes/person/[personid]/+page.ts
@@ -3,13 +3,13 @@ import type { PageLoadEvent } from './$types';
 export async function load({ parent, params }: PageLoadEvent) {
 	const { supabase } = await parent();
 
-	const { data, error } = await supabase
+	const { data: orgs } = await supabase
 		.from('orgs')
 		.select(`id, name, paths, profiles!profiles_orgid_fkey(personid, name)`)
 		.not('profiles', 'is', null)
 		.eq('profiles.personid', params.personid);
 
 	return {
-		orgs: error ? null : data
+		orgs
 	};
 }
